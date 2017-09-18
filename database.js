6d9576714ce1b9c3bfd19afb1423837b1d6106ab
ajliss <aaron@lissismore.com>
2017-09-18 12:40:26 -0700

    .gitignore fix

--- database.js
+++ database.js
@@ -1,6 +1,15 @@
 const Sequelize = require('sequelize');
+
+
 const sequelize = new Sequelize('Basketball', 'root', '', {
   host: '127.0.0.1:8000',
   dialect: 'sqlite'
   storage: './bball.sqlite'
 });
+
+const Players = sequelize.define('players', {
+  name: {
+    type: Sequelize.STRING
+  },
+  
+})
