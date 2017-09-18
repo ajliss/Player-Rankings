f2a9259f39fa7e73a40e5c025edf41b7ccfd6b91
ajliss <aaron@lissismore.com>
2017-09-18 15:31:27 -0700

    Database started.

--- database.js
+++ database.js
@@ -1,9 +1,8 @@
 const Sequelize = require('sequelize');
 
-
 const sequelize = new Sequelize('Basketball', 'root', '', {
   host: '127.0.0.1:8000',
-  dialect: 'sqlite'
+  dialect: 'sqlite',
   storage: './bball.sqlite'
 });
 
@@ -11,5 +10,31 @@ const Players = sequelize.define('players', {
   name: {
     type: Sequelize.STRING
   },
-  
-})
+  age: {
+    type: Sequelize.INTEGER
+  },
+  position: {
+    type: Sequelize.STRING
+  },
+});
+
+const Averages = sequelize.define('avgs', {
+  ppg: {
+    type: Sequelize.DOUBLE
+  },
+  rpb: {
+    type: Sequelize.DOUBLE
+  },
+  apg: {
+    type: Sequelize.DOUBLE
+  },
+
+});
+
+Averages.belongsTo(Players);
+
+Players.sync();
+Averages.sync();
+
+module.exports.players = Players;
+module.exports.avgs = Averages;
--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 const express = require('express');
-
+const parser = require('body-parser')
+const db = require('./database')
 
 const server = express();
 
 server.get('/', (req, res) => {
   res.send('Got \'em');
+});
+
+server.get('/players', (req, res) => {
+  if (req.body.name) {
+    // get player by name
+  } else {
+    // get all players
+  }
+  res.status(200).send(data)
+});
+
+server.get('/averages', (req, res) => {
+  if (req.body.name) {
+    // get player and averages by name
+  } else {
+    // get all players and averages
+  }
+  res.status(200).send(data)
+});
+
+server.post('/players', (req, res) => {
+  // create player instance and put it into db
+  res.status(201).send('posted and toasted');
 })
+
+server.post('/averages', (req, res) => {
+  // create average instance and put it into db
+  res.status(201).send('posted and toasted');
+})
+
 
 server.listen(8000, () => {
   console.log('Server listening at port 8000');
