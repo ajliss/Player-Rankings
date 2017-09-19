b6302dfe832aae04f87e95f1b7ea371f3bce9369
ajliss <aaron@lissismore.com>
2017-09-19 15:22:19 -0700

    Toggles between data and submit form on client.

--- client/components/App.js
+++ client/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import PlayersList from './PlayersList';
 import Input from './Input';
 import NavBar from './NavBar';
+import Player from './Player'
 
 class App extends React.Component {
   constructor() {
@@ -10,36 +12,65 @@ class App extends React.Component {
     this.state = {
       players: [],
       averages: [],
+      showForm: false
     }
   }
 
   componentDidMount() {
-    this.getPlayers().then(response => {
+    this.showPlayers();
+  }
+
+  showPlayers() {
+    axios.get('/players').then(response => {
       this.setState({
-        players: response.data
+        players: response.data,
+        showForm: false
       })
     });
   }
 
-  getPlayers() {
-    return axios.get('/players');
-  }
+  showAvgs() {
+    console.log('avgs')
+    axios.get('/averages').then(response => {
+      this.setState({
+        avgs: response.data
+      })
+    });
+  } 
 
-  getAverages() {
-    return axios.get('/averages');
+  toggleForm() {
+    this.setState({showForm: true})
   }
 
   render() {
     return (
       <div>
         <div>
-          <NavBar/>
-          <Input/>
+          <NavBar 
+            showPlayers={this.showPlayers.bind(this)} 
+            showAvgs={this.showAvgs.bind(this)}
+            toggle={this.toggleForm.bind(this)}
+          />
         </div>
-        <div>Swooping and Hooping</div>
-        <PlayersList players={this.state.players}/>
+        { this.state.showForm ? <Input /> : <PlayersList players={this.state.players}/> }
       </div>
     )
   }
 }
 export default App;
+
+  // render() {
+  //   return (
+  //     <Router>
+  //       <div>
+  //         <ul>
+  //           <li><Link to="/players">Players</Link></li>
+  //         </ul>
+
+  //         <hr/>
+
+  //         <Route path="/players" component={PlayersList}/>
+  //       </div>
+  //     </Router>
+  //   )
+  // }
--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -5,9 +5,9 @@ const NavBar = (props) => {
   return (
     <div>
        <Nav bsStyle="tabs" activeKey="1">
-        <NavItem eventKey="1" href="/players">Players</NavItem>
-        <NavItem eventKey="2" href="/averages">Averages</NavItem>
-        <NavItem eventKey="3" href="players">Add a Player</NavItem>
+        <NavItem onClick={props.showPlayers.bind(this)}>Players</NavItem>
+        <NavItem onClick={props.showAvgs.bind(this)}>Averages</NavItem>
+        <NavItem onClick={props.toggle.bind(this)}>Add a Player</NavItem>
         <NavItem eventKey="3" href="/averages">Add Stats</NavItem>
        </Nav>
     </div>
